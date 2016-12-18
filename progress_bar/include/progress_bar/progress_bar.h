1d8538770f7368bf477b95dd14b86dd0f8e61c7b
Artyom Katsap <artyom.katsap@picscout.com>
2016-12-18 09:10:39 +0200

    initial commit

---
+++ progress_bar/include/progress_bar/progress_bar.h
@@ -0,0 +1,28 @@
+#ifndef _PROGRESS_BAR_H_
+#define _PROGRESS_BAR_H_
+
+
+
+#ifdef _WIN32
+#ifdef PROGRESS_BAR_EXPORTS 
+#define PROGRESS_BAR_API __declspec(dllexport)
+#else
+#define PROGRESS_BAR_API __declspec(dllimport)
+#endif
+#else
+#define PROGRESS_BAR_API 
+#endif
+
+#include <string>
+#include <functional>
+
+
+const int STOP = 1;
+
+void PROGRESS_BAR_API run_progress_bar(const std::string &text, std::function<float()> &percentage_function);
+
+void PROGRESS_BAR_API run_busy_animation(const std::string &text, bool &monitor);
+
+
+
+#endif
---
+++ progress_bar/src/progress_bar.cpp
@@ -0,0 +1,43 @@
+// progress_bar.cpp : Defines the exported functions for the DLL application.
+//
+
+#include "stdafx.h"
+#include "progress_bar.h"
+
+#include <thread>
+#include <iostream>
+
+using std::string;
+
+static const std::string states[4] = {"|", "/", "--", "\\"};
+
+void run_progress_bar(const std::string& text, std::function<float()>& percentage_function)
+{
+	std::thread* th = new std::thread([&percentage_function, text, th]()
+	{
+		auto perc = 0.0;
+		while (perc < 1.)
+		{
+			printf("\r%s [%-50s] %d %%", text.c_str(), std::string(perc * 50, '#').c_str(), int(perc * 100));
+			std::this_thread::sleep_for(std::chrono::milliseconds(500));
+			perc = percentage_function();
+		}
+		delete th;
+	});
+}
+
+void run_busy_animation(const std::string &text, bool &monitor)
+{
+	std::thread* th = new std::thread([&monitor, text, th]()
+	{
+		int state = 0;
+		while (monitor)
+		{	
+			printf("\r%s%s ", text.c_str(),states[state++].c_str());
+			std::this_thread::sleep_for(std::chrono::milliseconds(500));
+			if (state >= 4) state = 0;
+			
+		}
+		delete th;
+	});
+}
